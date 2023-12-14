988c30de555433b43f0c9d544f3aba345f09bcba
Elijah Lekomo <lekomoayomide@gmail.com>
2023-12-14 17:09:29 +0200

    - Reduced scroll reveal time. - Added new images to img folder. - Changed home section car image. -Changed background color to main color when home section as a width of 568px.

--- js/main.js
+++ js/main.js
@@ -13,16 +13,16 @@ window.onscroll = () => {
 
 const sr =ScrollReveal ({
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 1800,
+    delay: 300,
     // reset: true,
 })
  
-sr.reveal('.text',{delay: 150, origin: 'top'})
-sr.reveal('.form-container form', {delay: 500, origin: 'left'})
-sr.reveal('.heading', {delay: 500, origin: 'top'})
-sr.reveal('.ride container .box', {delay: 500, origin: 'top'})
-sr.reveal('.services-container .box', {delay: 500, origin: 'top'})
-sr.reveal('.about container .box', {delay: 500, origin: 'top'})
-sr.reveal('.reviews container', {delay: 500, origin: 'top'})
+sr.reveal('.text',{delay: 100, origin: 'top'})
+sr.reveal('.form-container form', {delay: 300, origin: 'left'})
+sr.reveal('.heading', {delay: 300, origin: 'top'})
+sr.reveal('.ride container .box', {delay: 300, origin: 'top'})
+sr.reveal('.services-container', {delay: 200, origin: 'top'})
+sr.reveal('.about container .box', {delay: 300, origin: 'top'})
+sr.reveal('.reviews container', {delay: 300, origin: 'top'})
 sr.reveal('.newsletter .box', {delay: 300, origin: 'bottom'})
